be48288605fb2efc3f4a8321dbfa1c9f12d26866
Travis <Travis@172.18.1.143>
2017-03-04 14:34:27 -0500

    Added 2nd User class

---
+++ src/java/mainpackage/User.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainpackage;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Travis
+ */
+@ApplicationScoped
+@Named
+public class User {
+    
+}
