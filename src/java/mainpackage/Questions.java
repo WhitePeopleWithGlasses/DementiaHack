d66a6f8e5ed8ac50332cf14f1cd53c0d04cf08cb
Michael <Michael@Michael_Snopko>
2017-03-04 16:20:36 -0500

    Hope this works - Mike

--- src/java/mainpackage/Questions.java
+++ src/java/mainpackage/Questions.java
@@ -5,10 +5,15 @@
  */
 package mainpackage;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
 /**
  *
  * @author Travis
  */
+@ApplicationScoped
+@Named
 public class Questions {
     
 }
