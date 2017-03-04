a7871fbb1d3e29b32382dda0df0f8e1c5b25614e
Travis <Travis@172.18.1.143>
2017-03-04 14:40:53 -0500

    Added Questions and QuestionsLog class, declared variables in QuestionsLog

---
+++ src/java/mainpackage/QuestionsLog.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainpackage;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Travis
+ */
+public class QuestionsLog {
+    private int questionId;
+    private String question;
+    private String correctAnswer;
+    private String userAnswer;
+    private Date date;
+    
+}
